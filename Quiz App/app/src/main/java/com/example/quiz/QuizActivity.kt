34b16efc8e967e9441746e0a4cd18e782d85367e
Kunjesh <kunjpanchal2414@gmail.com>
2024-02-19 16:00:31 +0530

    Quiz Activity modified

--- Quiz App/app/src/main/java/com/example/quiz/QuizActivity.kt
+++ Quiz App/app/src/main/java/com/example/quiz/QuizActivity.kt
@@ -4,10 +4,14 @@ import QuestionModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.quiz.databinding.ActivityQuizBinding
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity(),View.OnClickListener {
 
     companion object{
         var questionModelList : List<QuestionModel> = listOf()
@@ -16,18 +20,29 @@ class QuizActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityQuizBinding
     var currQuestionIndex = 0;
+    var selectedAnswer = ""
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            Btn0.setOnClickListener(this@QuizActivity)
+            Btn1.setOnClickListener(this@QuizActivity)
+            Btn2.setOnClickListener(this@QuizActivity)
+            Btn3.setOnClickListener(this@QuizActivity)
+            NextBtn.setOnClickListener(this@QuizActivity)
+        }
+
         loadQuestions()
         startTimer()
     }
 
     private fun startTimer() {
-        val TotalTimeInMillis = time.toInt() * 60 * 1000L
-        object : CountDownTimer(TotalTimeInMillis, 1000L){
+        val totalTimeInMillis = time.toInt() * 60 * 1000L
+        object : CountDownTimer(totalTimeInMillis, 1000L){
             override fun onTick(msUntilFinished: Long) {
                 val seconds = msUntilFinished/1000
                 val minutes = seconds/60
@@ -43,6 +58,12 @@ class QuizActivity : AppCompatActivity() {
     }
 
     private fun loadQuestions(){
+
+        if(currQuestionIndex== questionModelList.size){
+            finishQuiz()
+            return
+        }
+
         binding.apply {
             qIndicator.text = "Question ${currQuestionIndex+1} / ${questionModelList.size}"
             qProgressIndicator.progress =
@@ -54,5 +75,41 @@ class QuizActivity : AppCompatActivity() {
             Btn3.text = questionModelList[currQuestionIndex].options[3]
 
         }
+    }
+
+    override fun onClick(view: View?) {
+
+        binding.apply{
+            Btn0.setBackgroundColor(getColor(R.color.gray))
+            Btn1.setBackgroundColor(getColor(R.color.gray))
+            Btn2.setBackgroundColor(getColor(R.color.gray))
+            Btn3.setBackgroundColor(getColor(R.color.gray))
+        }
+
+        val clickedBtn = view as Button
+        if(clickedBtn.id==R.id.Next_Btn){
+
+            // No option selected
+            if(selectedAnswer.isEmpty()){
+                Toast.makeText(applicationContext, "Select an Answer to Continue", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            // Next Btn is clicked
+            if(selectedAnswer == questionModelList[currQuestionIndex].correct){
+                score++
+                Log.i("Score of Quiz ", score.toString())
+            }
+            currQuestionIndex++
+            loadQuestions()
+        }else{
+            // Option Btn is clicked
+            selectedAnswer = clickedBtn.text.toString()
+            clickedBtn.setBackgroundColor(getColor(R.color.orange))
+        }
+    }
+
+    private fun finishQuiz(){
+
     }
 }
